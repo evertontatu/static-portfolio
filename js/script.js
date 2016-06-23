03cdc8ab6db12b1b120cfeab3c4a1d238b3da087
Everton Ferreira <evertonferreira.de@gmail.com>
2016-06-23 15:11:58 -0300

    Gulp basico configurado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-
-
-$(function(){
-	$("h1").addClass("fonte");
-};
+$('.nav-toggle').click(function() {
+	var altura = $('.nav ul').height();
+	if($('.nav').hasClass('fechada')) {
+		$('.nav').animate({height:altura},{queue:false, duration:200}).removeClass('fechada');
+	}
+	else {
+		$('.nav').animate({height:'0px'},{queue:false, duration:200}).addClass('fechada');
+	}
+});
