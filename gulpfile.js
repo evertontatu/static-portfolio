8d524a8395b2ddc1fd86bfa271a1f5225a4f180c
Everton Ferreira <evertonferreira.de@gmail.com>
2016-06-26 10:03:11 -0300

    gulp-ruby-sass works

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,26 @@
-// Aqui nós carregamos o gulp e os plugins através da função `require` do nodejs
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
 
-// Definimos o diretorio dos arquivos para evitar repetição futuramente
-var files = "./js/*.js";
-
-//Aqui criamos uma nova tarefa através do ´gulp.task´ e damos a ela o nome 'lint'
-gulp.task('lint', function() {
-
-// Aqui carregamos os arquivos que a gente quer rodar as tarefas com o `gulp.src`
-// E logo depois usamos o `pipe` para rodar a tarefa `jshint`
-gulp.src(files)
-.pipe(jshint())
-.pipe(jshint.reporter('default'));
+gulp.task('scripts', function () {
+    gulp.src('js/**/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
 });
 
-//Criamos outra tarefa com o nome 'dist'
-gulp.task('dist', function() {
-
-// Carregamos os arquivos novamente
-// E rodamos uma tarefa para concatenação
-// Renomeamos o arquivo que sera minificado e logo depois o minificamos com o `uglify`
-// E pra terminar usamos o `gulp.dest` para colocar os arquivos concatenados e minificados na pasta build/
-gulp.src(files)
-.pipe(concat('./dist'))
-.pipe(rename('script.min.js'))
-.pipe(uglify())
-.pipe(gulp.dest('./dist'));
+gulp.task('sass', function () {
+  return
+    sass('_sass/style.scss')
+      .pipe(autoprefixer())
+      .pipe(minifyCss())
+      .pipe(gulp.dest('dist'));
 });
 
-//Criamos uma tarefa 'default' que vai rodar quando rodamos `gulp` no projeto
-gulp.task('default', function() {
-
-// Usamos o `gulp.run` para rodar as tarefas
-// E usamos o `gulp.watch` para o Gulp esperar mudanças nos arquivos para rodar novamente
-gulp.run('lint', 'dist');
-gulp.watch(files, function(evt) {
-gulp.run('lint', 'dist');
-});
+gulp.task('default', ['scripts', 'sass'], function () {
+    gulp.watch('js/**/*.js', ['scripts']);
+    gulp.watch('_sass/*.{sass,scss}', ['sass']);
 });
