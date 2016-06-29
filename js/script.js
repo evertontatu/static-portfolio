4c54d28459a556d8a1d40e1d1cbdfba79d7674da
Everton Ferreira PC <evertonferreira.de@gmail.com>
2016-06-29 19:14:20 -0300

    widthDevice HeightDevice

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-$('.nav-toggle').click(function() {
-	var altura = $('.nav ul').height();
-	if($('.nav').hasClass('fechada')) {
-		$('.nav').animate({height:altura},{queue:false, duration:200}).removeClass('fechada');
-	}
-	else {
-		$('.nav').animate({height:'0px'},{queue:false, duration:200}).addClass('fechada');
-	}
+$(function(){
+	$('.nav-toggle').click(function() {
+		var altura = $('.nav ul').height();
+		if($('.nav').hasClass('fechada')) {
+			$('.nav').animate({height:altura},{queue:false, duration:200}).removeClass('fechada');
+		}
+		else {
+			$('.nav').animate({height:'0px'},{queue:false, duration:200}).addClass('fechada');
+		}
+	});
+});
+
+$(function(){
+	var widthDevice = $(window).width();
+	var heightDevice = $(window).height();
+
+	$('.page').css('height', heightDevice);
+	$('.page').css('width', widthDevice);
 });
