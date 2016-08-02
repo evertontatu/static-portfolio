985c41db989e5bdc6a4a28897b99272a5d91f6a0
Everton Ferreira <evertonferreira.de@gmail.com>
2016-08-01 22:05:05 -0300

    Form01: Modal Form com loading e resposta

--- js/script.js
+++ js/script.js
@@ -25,3 +25,51 @@ $(function(){
 	})
 
 });
+
+
+
+//SCROLL DOWN PAGE
+$(document).ready(function(){
+    $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top-10
+        }, 800, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+
+//MODAL CONTACT
+$(function(){
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on the button, open the modal
+	btn.onclick = function() {
+	    modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+});
