670d660a2c9124b79168de15b32752fc33f25235
Everton Ferreira <evertonferreira.de@gmail.com>
2016-08-02 21:32:52 -0300

    Adicionado link para as section

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
         $target = $(target);
 
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top-10
+            'scrollTop': $target.offset().top-0
         }, 800, 'swing', function () {
             window.location.hash = target;
         });
