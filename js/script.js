fbc70a52e4f92f654c7acc7afdb3e5378dc19830
Everton Ferreira <evertonferreira.de@gmail.com>
2016-09-26 22:39:11 -0300

    add style scss form input on contact

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//MENU NAV OPEN OR CLOSE DEPEND WIDTH DEVICE AND CLICK 
 $(function(){
 	$('.nav-toggle').click(function() {
 		var altura = $('.nav ul').height();
@@ -18,6 +19,7 @@ $(function(){
 	$('.home').css('height', heightDevice);
 });
 
+//CLICK MENU MOBILE
 $(function(){
 
 	$('.menu-mobile').click(function(e) {
@@ -61,26 +63,18 @@ $(document).ready(function(){
 
 //MODAL CONTACT
 $(function(){
-	// Get the modal
 	var modal = document.getElementById('myModal');
-
-	// Get the button that opens the modal
 	var btn = document.getElementById("myBtn");
-
-	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
 
-	// When the user clicks on the button, open the modal
 	btn.onclick = function() {
 	    modal.style.display = "block";
 	}
 
-	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
 	    modal.style.display = "none";
 	}
 
-	// When the user clicks anywhere outside of the modal, close it
 	window.onclick = function(event) {
 	    if (event.target == modal) {
 	        modal.style.display = "none";
@@ -93,7 +87,8 @@ $(function(){
 	var posicaoInicial = $('.menu').position().top;
 
 	$(document).scroll(function () {
-    	var posicaoScroll = $(document).scrollTop(); // obtem a quantidade de scroll no momento
+    	var posicaoScroll = $(document).scrollTop();
+
     	if (posicaoScroll > '170') {
 				$('.menu').addClass('menuBG','menuHeigth');
 				$('.logoMenu').addClass('menuHeigth');
